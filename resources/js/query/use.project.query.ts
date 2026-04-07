ebb868ee3ffabd8a2f4e346cbe97c195d3eee793
Igor Yuzkiv <igor97@gmail.com>
2026-04-07 13:22:18 +0300

    projects-crud

--- resources/js/query/use.project.query.ts
+++ resources/js/query/use.project.query.ts
@@ -3,12 +3,11 @@ import { computed, toValue } from 'vue'
 import type { MaybeRefOrGetter } from 'vue'
 import { projectsApi } from '@/api/projects.api'
 import { projectsKeys } from '@/config'
-import { Maybe } from '@/types/result.types'
 
-export function useProjectQuery(projectId: MaybeRefOrGetter<Maybe<string>>) {
+export function useProjectQuery(projectId: MaybeRefOrGetter<string>) {
     return useQuery({
-        queryKey: computed(() => projectsKeys.detail(toValue(projectId)!)),
-        queryFn: () => projectsApi.getById(toValue(projectId)!),
+        queryKey: computed(() => projectsKeys.detail(toValue(projectId))),
+        queryFn: () => projectsApi.getById(toValue(projectId)),
         enabled: computed(() => !!toValue(projectId)),
     })
 }
